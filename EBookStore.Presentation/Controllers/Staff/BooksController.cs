9e3bb78e27834a067ae2664a0d3864240fb0b470
Tamnx <xuantamjee413@gmail.com>
2025-03-24 18:18:10 +0700

    Staff: Manage Book

---
+++ EBookStore.Presentation/Controllers/Staff/BooksController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EBookStore.Core.Models;
+using EBookStore.Business.IServices;
+
+namespace EBookStore.Presentation.Controllers.Staff
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookService.GetAllBooksAsync();
+            return View(books);
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _bookService.GetBookByIdAsync(id.Value);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_bookService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        // POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Author,CategoryId,Description,Isbn,PublishedYear,ImageUrl")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                await _bookService.AddBookAsync(book);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_bookService.GetCategories(), "Id", "Name", book.CategoryId);
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _bookService.GetBookByIdAsync(id.Value);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_bookService.GetCategories(), "Id", "Name", book.CategoryId);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Author,CategoryId,Description,Isbn,PublishedYear,ImageUrl")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _bookService.UpdateBookAsync(book);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_bookService.GetCategories(), "Id", "Name", book.CategoryId);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _bookService.GetBookByIdAsync(id.Value);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _bookService.DeleteBookAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> BookExists(int id)
+        {
+            return await _bookService.BookExistsAsync(id);
+        }
+    }
+}
--- EBookStore.Presentation/obj/Debug/net8.0/EBookStore.Presentation.AssemblyInfo.cs
+++ EBookStore.Presentation/obj/Debug/net8.0/EBookStore.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EBookStore.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e9b3917751d7d42627eade83a9c4aaccb6f7cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45fad28c49f079c003f7a63f0ce0023a4191e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EBookStore.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EBookStore.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
